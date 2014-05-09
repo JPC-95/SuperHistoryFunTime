b6b6aa2c4ff4db14879367ae50b2d3c539a7a07f
unknown <chc238u11@CHC238W11.Instruction.network.bcc>
2014-05-08 19:44:03 -0700

    first upload

---
+++ www/js/index.js
@@ -0,0 +1,19 @@
+//This is the main JavaScript source for Super History Fun Time
+$("button.camera-control").click(function () {
+	// Navigtor is PhoneGap access to hardware.
+	if (navigator.camera) {
+		var options = {
+		quality: 60,
+		destinationType: Camera.DestinationType.DATA_URL,
+		sourceType: 0;
+		encodingType: 0;
+		};
+		navigator.camera.getPicture(getPhoto, null, options);
+	}
+})
+
+
+
+function getPhoto (data) {
+	$("#camera-photo").attr("src", "data:image/jpeg;base64," + data);
+}
