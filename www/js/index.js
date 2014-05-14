4d9d90aa496e4e6f1211228cf9edd68cc9bd9cec
unknown <chc238u12@CHC238W12.Instruction.network.bcc>
2014-05-13 19:25:24 -0700

    We added long and lat of the photo

--- www/js/index.js
+++ www/js/index.js
@@ -5,10 +5,11 @@ $("button.camera-control").click(function () {
 		var options = {
 		quality: 60,
 		destinationType: Camera.DestinationType.DATA_URL,
-		sourceType: 0,
+		sourceType: 1,
 		encodingType: 0
 		};
 		navigator.camera.getPicture(getPhoto, null, options);
+		navigator.geolocation.getCurrentPosition(getPosition, null, {enableHighAccuracy: true});
 	}
 })
 
@@ -16,4 +17,12 @@ $("button.camera-control").click(function () {
 
 function getPhoto (data) {
 	$("#camera-photo").attr("src", "data:image/jpeg;base64," + data);
+}
+
+function getPosition (position) {
+	var longitude = position.coords.longitude;
+	var latitude = position.coords.latitude;
+	
+	$("#longitude").html("Long: " + longitude);
+	$("#latitude").html("Lat: " + latitude);
 }
